b39cd48b3644fd7ecb9dfdd73466848c6c47be7c
Anna <kaletyuk@yandex.ru>
2020-05-14 16:49:02 +0300

    Slider

--- js/script.js
+++ js/script.js
@@ -114,9 +114,9 @@ window.addEventListener('DOMContentLoaded', function () {
         input = form.getElementsByTagName('input'),
         statusMessage = document.createElement('div');
 
-        statusMessage.classList.add('status');
+    statusMessage.classList.add('status');
 
-    form.addEventListener('submit', function(event) {
+    form.addEventListener('submit', function (event) {
         event.preventDefault();
         form.appendChild(statusMessage);
 
@@ -127,17 +127,17 @@ window.addEventListener('DOMContentLoaded', function () {
         let formData = new FormData(form);
 
         let obj = {};
-        formData.forEach(function(value, key) {
+        formData.forEach(function (value, key) {
             obj[key] = value;
         });
         let json = JSON.stringify(obj);
 
         request.send(json);
 
-        request.addEventListener('readystatechange', function() {
+        request.addEventListener('readystatechange', function () {
             if (request.readyState < 4) {
                 statusMessage.innerHTML = message.loading;
-            } else if(request.readyState === 4 && request.status == 200) {
+            } else if (request.readyState === 4 && request.status == 200) {
                 statusMessage.innerHTML = message.success;
             } else {
                 statusMessage.innerHTML = message.failure;
@@ -147,7 +147,58 @@ window.addEventListener('DOMContentLoaded', function () {
         for (let i = 0; i < input.length; i++) {
             input[i].value = '';
         }
+
+
+
     });
 
 
+    //Slider
+
+    let slideIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+
+    showSlides(slideIndex);
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach((item) => item.style.display = 'none');
+        dots.forEach((item) => item.classList.remove('dot-active'));
+
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+    function currentSlides(n) {
+        showSlides(slideIndex = n);
+    }
+
+    prev.addEventListener('click', function() {
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', function() {
+        plusSlides(1);
+    })
+
+    dotsWrap.addEventListener('click', function(event) {
+        for (let i = 0; i < dots.length + 1; i++) {
+            if (event.target.classList.contains('dot') && event.target == dots[i-1]) {
+                currentSlides(i);
+            }
+        }
+    })
 });
